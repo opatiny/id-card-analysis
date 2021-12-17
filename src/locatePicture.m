161b0ffbc9fe35203cd1cbde2215eb45f94f9898
Oceane Patiny <oceane@patiny.com>
2021-12-17 17:37:52 +0100

    minor changes

--- src/locatePicture.m
+++ src/locatePicture.m
@@ -1,18 +1,19 @@
 %% Locate the picture in an ID card
 
-function boundingBoxes = locatePicture(id)
-    gray = im2gray(id);
+function boundingBoxes = locatePicture(image)
+    gray = im2gray(image);
     hist = imhist(gray);
     adj = imadjust(hist);
     threshold = otsuthresh(adj);
+    % default threshold used in imbinarize is otsu
     % apparently imbinarize() is better than im2bw()
     bw = imbinarize(adj, threshold);
     inverted = ~bw;
     stats = regionprops(inverted, 'BoundingBox','Area');
     [v,i] = maxk([stats.Area], 3);
     % preallocating memory for speed
-    boundingBoxes = cell(1,3);
-   for index = 1:3
-       boundingBoxes(index) = stats.BoundingBox(i);
-   end
+    % boundingBoxes = zeros(1,3);
+    for index =  1 : length(i)
+        boundingBoxes(index) = stats(i(index)).BoundingBox;
+    end
 end
