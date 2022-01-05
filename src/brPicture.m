b0a369cf0589297699b26c7cb98fdadefaa6288a
Oceane Patiny <oceane@patiny.com>
2022-01-05 11:51:07 +0100

    docs: add function doc

--- src/brPicture.m
+++ src/brPicture.m
@@ -1,15 +1,22 @@
 %% Show the boundary rectangle of the picture of an ID card
 
-function [result, largeOnly] = brPicture(image)
+function [result, mask] = brPicture(image)
+% brPicture: Draw the boundary rectangle of the picture in an ID card scan.
+% Inputs:
+%   - image: the image to process
+% Outputs:
+%   - result: the image with the boundary rectangle
+%   - mask: the mask of the image used to compute the BR
     %% create mask
     gray = im2gray(image);
     adj = imadjust(gray);
     bw = imbinarize(adj);
     inverted = ~bw;
-    largeOnly = bwareaopen(inverted, 100)
+    % remove small objects areas
+    largeOnly = bwareaopen(inverted, 100);
     imshow(largeOnly);
     
-
+    mask = largeOnly;
     %% find regions of interest
     % number of largest area regions to keep
     N = 1;
@@ -19,7 +26,7 @@ function [result, largeOnly] = brPicture(image)
     %% add ROI to image
    result = image;
    
-    for i = 1 : length(maxIndexes)
+    for i = 1 : N
         result = insertShape(result,'rectangle',stats(maxIndexes(i)).BoundingBox, 'Color','r', 'LineWidth',3);
     end
 end
