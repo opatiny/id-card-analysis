f950de3cd15a4660b96586797f39fbbc4c56f5a3
Oceane Patiny <oceane@patiny.com>
2022-01-06 11:10:49 +0100

    fix: continue detectOrientation (#5)

--- src/detectOrientation.m
+++ src/detectOrientation.m
@@ -1,18 +1,39 @@
-function angle = detectOrientation(image, pictureLocation)
+function angle = detectOrientation(image)
 % detectOrientation: return the angle by which the image has to be rotated
 %                    to be oriented properly
 % Inputs:
 %    - image: the image to process
-%    - pictureLocation: estimated picture location (bounding box)
 % Outputs:
-%    - angle: angle by which to rotate
+%    - angle: angle by which to rotate counterclockwise
+
+pictureLocation = detectPicture(image);
 
 if isequal(pictureLocation, zeros(4,1))
     disp('Picture location is not defined.');
-    
+    angle = 0;
+    return;
 end
 
 [imHeight, imWidth] = size(image);
+[x, y, picWidth, picHeight] = pictureLocation(1);
+
+if imHeight < imWidth
+    if x + picWidth < imWidth/2
+        angle = 0;
+        return;
+    else 
+        angle = 180;
+        return;
+    end
+else
+    if y + picHeight < imHeight/2
+        angle = 270;
+        return;
+    else
+        angle = 90;
+        return;
+    end
+end
 
 end
 
